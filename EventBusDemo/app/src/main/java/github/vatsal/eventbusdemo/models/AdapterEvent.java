59623856f65698c738b1d549d9341028130833e1
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-08-05 16:18:14 +0530

    feat: Added EventBus model

---
+++ EventBusDemo/app/src/main/java/github/vatsal/eventbusdemo/models/AdapterEvent.java
@@ -0,0 +1,17 @@
+package github.vatsal.eventbusdemo.models;
+
+
+/**
+ * Created by
+ * --Vatsal Bajpai on
+ * --05/08/16 at
+ * --4:17 PM
+ */
+public class AdapterEvent {
+
+    public String name;
+
+    public AdapterEvent(String name) {
+        this.name = name;
+    }
+}
